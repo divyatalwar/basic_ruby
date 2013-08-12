f7a58e71cbb628f5d1d184e9760a439272a9b30a
divya <divya@vinsol.com>
2013-08-12 09:20:19 +0530

    indentation proper

--- fibonnaci.rb
+++ fibonnaci.rb
@@ -4,14 +4,14 @@ def fibonacci_series
   printf("fibonacci series :%s", "#{first_number} #{second_number}")
   while 
     third_number = first_number + second_number
-    yield(third_number,1000)
+    yield(third_number, 1000)
     first_number = second_number
     second_number = third_number
   end
 end
 fibonacci_series do
-|series ,max_limit|
+|series , max_limit|
 break if series > max_limit
-print " #{series}"
+print "#{series}"
 end
 puts "\n"
