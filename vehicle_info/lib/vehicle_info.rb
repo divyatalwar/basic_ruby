254f8cd17aabd83aead5c09ddd701af9e9522164
divya <divya@vinsol.com>
2013-08-13 13:53:55 +0530

    review

---
+++ vehicle_info/lib/vehicle_info.rb
@@ -0,0 +1,20 @@
+class Vehicle
+  def initialize(name ,price)
+    @name = name
+    @price = price
+  end
+  def inspect
+    "name : #@name price : #@price "
+  end
+  attr_writer :price
+end
+class Bike < Vehicle
+  def initialize(name,price,dealer)
+    super(name,price)
+    @dealer = dealer
+  end
+  def inspect
+    super + " dealer : #@dealer"
+  end
+end
+
