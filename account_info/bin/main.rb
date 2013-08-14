f53318bf48075884e1375e30cea4a12eed4a3597
divya <divya@vinsol.com>
2013-08-14 10:22:39 +0530

    default value for balance

--- account_info/bin/main.rb
+++ account_info/bin/main.rb
@@ -1,5 +1,8 @@
-require_relative "account_info"
-account1 = Account.new("Divya");
+require_relative "../lib/account_info"
+account1 = Account.new("Divya", 2000);
 account1.deposit(500);
 account1.withdraw(250);
 puts account1.inspect
+account2 = Account.new("abc");
+account2.deposit(500);
+puts account2.inspect
--- account_info/lib/account_info.rb
+++ account_info/lib/account_info.rb
@@ -1,25 +1,24 @@
 class Account
   @@account_no = 0
-  def initialize(name)
+  def initialize(name, balance = 1000)
     @name = name
     @@account_no += 1
     @current_account_number = @@account_no
-    @balance = 1000
+    @balance = balance
   end
-  public
-    def deposit(amount)
-      @balance += amount
-      print "deposited ",amount," in account no. ", @current_account_number, "\n"
+  def deposit(amount)
+    @balance += amount
+    print "deposited ", amount ," in account no. ", @current_account_number, "\n"
+  end
+  def withdraw(amount)
+    if amount <= @balance
+      @balance -= amount
+      print "withdrawn ", amount , " from account no. ", @current_account_number, "\n"
+    else
+      print "You cannot withdraw. Your account balance is insufficient.\n"
     end
-    def withdraw(amount)
-      if amount <= @balance
-        @balance -= amount
-        print "withdrawn ",amount," from account no. ", @current_account_number, "\n"
-      else
-        print "You cannot withdraw. Your account balance is insufficient.\n"
-      end
-    end
-    def inspect
-      " #@name with account number #@current_account_number has balance #@balance"
-    end
+  end
+  def inspect
+    " #@name with account number #@current_account_number has balance #@balance"
+  end
 end
