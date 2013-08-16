9ba19b5bd5274122b427dc7562b15f4437ed6e1d
divya <divya@vinsol.com>
2013-08-16 10:30:13 +0530

    cope optimised,mthod name descriptive

--- array_to_hash/lib/array.rb
+++ array_to_hash/lib/array.rb
@@ -1,6 +1,6 @@
 class Array
   def convert_to_hash
-    new_hash  = Hash.new {|hash, key| hash[key] = [] }
+    new_hash  = Hash.new { |hash, key| hash[key] = [] }
     for element in self
       len = element.to_s.length
       new_hash[len] << element
