f54481aa5140543df5f9ccd7493238047cd67dc6
divya <divya@vinsol.com>
2013-08-17 18:17:55 +0530

    review

---
+++ sum_time2/lib/string.rb
@@ -0,0 +1,14 @@
+require "time"
+ class String
+  def valid?
+    time_format = /([0-1]?[0-9]|2[0-3]):([0-5][0-9]|60):([0-5][0-9]|60)/
+    return true if self =~ time_format 
+  end
+  def to_seconds
+    new_tym = Time.parse(self)
+    return ((new_tym.hour * 3600) + (new_tym.min * 60) + new_tym.sec)
+  end
+  
+end
+
+
