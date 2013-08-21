5748c6cbd3eb91f37e3970997401980243d5a066
divya <divya@vinsol.com>
2013-08-21 11:20:10 +0530

    principal not an integer always

--- interest_diff/bin/main.rb
+++ interest_diff/bin/main.rb
@@ -5,8 +5,8 @@ principal = gets.chomp
 print "Enter the time in years: "
 time = gets.chomp
 interest = Interest.new do |x|
-  x.principal = principal.to_i
-  x.time = time.to_i
+  x.principal = principal.to_f
+  x.time = time.to_f
   end
 puts "Difference in the simple and compound interest = Rs #{interest.difference}"
 
