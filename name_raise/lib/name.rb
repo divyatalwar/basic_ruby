f264eabe61eeb9dbf48a532875fd464b50d1c250
divya <divya@vinsol.com>
2013-08-21 18:02:34 +0530

    review

--- name_raise/lib/name.rb
+++ name_raise/lib/name.rb
@@ -5,7 +5,7 @@ class Name
       @last_name = last_name
       raise 'Cannot be left empty' if ( @first_name.length == 0 || @last_name.length == 0 )
       raise 'First letter of your first name is not capital' if !(@first_name == @first_name.capitalize)
-      rescue Exception => exception_type
+    rescue Exception => exception_type
       print "invalid :" , exception_type , "\n"     
     end
   end
