1d5522185915cafb971a28212d5e2a70a2f0b7b6
divya <divya@vinsol.com>
2013-08-21 11:20:37 +0530

    separate methods for simple and compund interest

--- interest_diff/lib/interest.rb
+++ interest_diff/lib/interest.rb
@@ -5,9 +5,13 @@ class Interest
     yield self
     @rate = 10
   end
+  def simple_interest
+    @principal * @rate * @time / 100.0
+  end
+  def compound_interest
+    @principal * (1 + @rate/100.0) ** @time - @principal
+  end
   def difference
-    simple_interest = @principal * @rate * @time / 100.0
-    compound_interest =  @principal * (1 + @rate/100.0) ** @time - @principal
     (compound_interest - simple_interest).round(2)
   end
 end
