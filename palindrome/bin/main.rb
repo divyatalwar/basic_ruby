76d444db865bbcdac775a5301f2703a0010c1ca9
divya <divya@vinsol.com>
2013-08-21 22:47:58 +0530

    user keeps taking input till q/Q is entered

--- palindrome/bin/main.rb
+++ palindrome/bin/main.rb
@@ -3,7 +3,9 @@ class String
     downcase == downcase.reverse 
   end 
 end
-puts "Enter the string"
-input = gets.chomp
-return if input =~ /^[q]$/i
-print "input string is " , input.palindrome? ? " a palindrome" : "not a palindrome" ,"\n"
+while true
+  puts "Enter the string"
+  input = gets.chomp
+  break if input =~ /^[q]$/i
+  print "input string is " , input.palindrome? ? " a palindrome" : "not a palindrome" ,"\n"
+end
