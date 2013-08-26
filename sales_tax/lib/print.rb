5ec7fa29b0dd92f45ed2fcd8236c9155111f1ce5
divya <divya@vinsol.com>
2013-08-26 11:28:51 +0530

    review

--- sales_tax/lib/print.rb
+++ sales_tax/lib/print.rb
@@ -1,9 +1,9 @@
 class Print
   def self.display_table(header,products,grand_total)
-    table_width = 19 * header.count
+    table_width = 17 * header.count
     puts "".rjust(table_width,'*')
     header.each {|title| print "|",title.center(16) }
-    puts"\n"
+    puts "|\n"
     puts "".rjust(table_width,'*')
     products.each do |row|
       print "|" , row.name.center(16)
@@ -11,7 +11,7 @@ class Print
       print "|" , row.sales_tax.center(16)
       print "|" , row.price.to_s.center(16)
       print "|" , row.total.to_s.center(16)
-      puts "\n"
+      puts "|\n"
     end
     puts "".rjust(table_width,'*')
     puts "Grand Total : #{grand_total.to_s}".center(table_width)
