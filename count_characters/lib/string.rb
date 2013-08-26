6b702a3b2540d499650463a70ab4a991487d38ac
divya <divya@vinsol.com>
2013-08-26 11:07:37 +0530

    used switch case and hash instead of eturning a string

--- count_characters/lib/string.rb
+++ count_characters/lib/string.rb
@@ -4,11 +4,11 @@ class String
     lowercase_range, uppercase_range, digit_range, whitespaces = ('a'..'z'), ('A'..'Z'), ('0'..'9'), ' '
     each_char do |element|
       case element
-        when lowercase_range then hash["lowercase"] += 1
-        when uppercase_range then hash["uppercase"] += 1
-        when digit_range then hash["digits"] += 1
-        when whitespaces 
-        else hash["special_characters"] += 1
+      when lowercase_range then hash["lowercase"] += 1
+      when uppercase_range then hash["uppercase"] += 1
+      when digit_range then hash["digits"] += 1
+      when whitespaces 
+      else hash["special_characters"] += 1
       end
     end
     hash
