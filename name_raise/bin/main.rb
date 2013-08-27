a82e9fae25906d045d815f954613ef6191724a90
divya <divya@vinsol.com>
2013-08-27 10:04:11 +0530

    separate cases for nil and case of first name

--- name_raise/bin/main.rb
+++ name_raise/bin/main.rb
@@ -1,8 +1,14 @@
-
 require_relative '../lib/name'
 puts "Enter the first name"
 first_name = gets.chomp
 puts "Enter the last name"
 last_name = gets.chomp
+puts "\nCASE 1 : When keeping the first letter of the first name small ,ouput will be :\n"
+first_name = first_name.downcase
 name1 = Name.new(first_name, last_name)
 puts name1
+puts "\nCASE 2 : When keeping the first name to be nil, ouput will be :\n"
+first_name = nil
+name2 = Name.new(first_name, last_name)
+puts name2
+
