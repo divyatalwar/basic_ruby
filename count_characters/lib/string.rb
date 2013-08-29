4b2a1aa774e23e1eea656f7146274bc48982d64e
divya <divya@vinsol.com>
2013-08-29 14:00:53 +0530

    name for hash changed

--- count_characters/lib/string.rb
+++ count_characters/lib/string.rb
@@ -1,17 +1,17 @@
 class String
   def count_characters
-    hash = Hash.new(0)    
+    character_count = Hash.new(0)    
     lowercase_range, uppercase_range, digit_range, whitespaces = ('a'..'z'), ('A'..'Z'), ('0'..'9'), ' '
     each_char do |element|
       case element
-      when lowercase_range then hash["lowercase"] += 1
-      when uppercase_range then hash["uppercase"] += 1
-      when digit_range then hash["digits"] += 1
+      when lowercase_range then character_count["lowercase"] += 1
+      when uppercase_range then character_count["uppercase"] += 1
+      when digit_range then character_count["digits"] += 1
       when whitespaces 
-      else hash["special_characters"] += 1
+      else character_count["special_characters"] += 1
       end
     end
-    hash
+    character_count
   end
 end
 
