3f296ab3b5fdb243a8f26e14312671636d4c07ed
divya <divya@vinsol.com>
2013-08-29 14:07:51 +0530

    name changed

--- csv/bin/main.rb
+++ csv/bin/main.rb
@@ -1,7 +1,7 @@
 require_relative "../lib/csv"
 include Csv
 puts "Reading a csv file...."
-employee_info = read_csv("#{ File.dirname(__FILE__) }/sample.csv")
+employee_info = read_from_csv("#{ File.dirname(__FILE__) }/sample.csv")
 puts "Writing to a new text file ..."
-write_csv("#{ File.dirname(__FILE__) }/output.txt", employee_info)
+write_to_text("#{ File.dirname(__FILE__) }/output.txt", employee_info)
 
--- csv/lib/csv.rb
+++ csv/lib/csv.rb
@@ -1,7 +1,7 @@
 require "csv"
 require_relative "employee"
 module Csv
-  def read_csv(file_name)
+  def read_from_csv(file_name)
     employee_details = []
     CSV.foreach(ARGV.shift || file_name) do |row|
       name, id, designation = row.each { |word|  word }
@@ -10,7 +10,7 @@ module Csv
     employee_details
   end
 
-  def write_csv(file_name, employee_info)
+  def write_to_text(file_name, employee_info)
     File.open(file_name, "w") do |file|
       employee_info.group_by { |employee| employee.designation }.sort.each do |key, value|
         file.puts key + 's'
