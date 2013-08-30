9684822c552396a248726f096326d65929ca0876
divya <divya@vinsol.com>
2013-08-30 19:04:15 +0530

    writing to a text file in main method

--- csv/bin/main.rb
+++ csv/bin/main.rb
@@ -1,7 +1,13 @@
 require_relative "../lib/csv"
-include Csv
 puts "Reading a csv file...."
-employee_info = read_from_csv("#{ File.dirname(__FILE__) }/sample.csv")
+employee_info = read_csv("#{ File.dirname(__FILE__) }/sample.csv")
 puts "Writing to a new text file ..."
-write_to_text("#{ File.dirname(__FILE__) }/output.txt", employee_info)
 
+File.open("#{ File.dirname(__FILE__) }/output.txt", "w") do |file|
+  employee_info.group_by { |employee| employee.designation }.sort.each do |key, value|
+    file.puts key + 's'
+    value.each do |emp |
+      file.puts "#{ emp.name }  (EmpId:#{emp.id}) "      
+    end
+  end
+end
--- csv/lib/csv.rb
+++ csv/lib/csv.rb
@@ -1,23 +1,12 @@
 require "csv"
 require_relative "employee"
-module Csv
-  def read_csv(file_name)
-    employee_details = []
-    CSV.foreach(ARGV.shift || file_name) do |row|
-      name, id, designation = row.each { |word|  word }
-      employee_details.push(Employee.new(name, id, designation))
-    end
-    employee_details
+def read_csv(file_name)
+  employee_details = []
+  CSV.foreach(ARGV.shift || file_name) do |row|
+    name, id, designation = row.each { |word|  word }
+    employee_details.push(Employee.new(name, id, designation))
   end
-
-  def write_to_file(file_name, employee_info)
-    File.open(file_name, "w") do |file|
-      employee_info.group_by { |employee| employee.designation }.sort.each do |key, value|
-        file.puts key + 's'
-        value.each do |emp |
-          file.puts "#{ emp.name }  (EmpId:#{emp.id}) "      
-        end
-      end
-    end
-  end
+  employee_details
 end
+
+
