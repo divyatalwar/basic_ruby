76eb698f5233d31ba9db1118cb7248afe7f2e277
DivyaTalwar <divya@vinsol.com>
2013-10-10 12:21:53 +0530

    returned the proper time format

--- sum_time/lib/time.rb
+++ sum_time/lib/time.rb
@@ -1,8 +1,11 @@
 require 'time'
 class Time
+
   def add_time(time)
     time_in_seconds = time.hour * 3600 + time.min * 60 + time.sec
     result_time = self + time_in_seconds
-    "Time after addition is: \n" + ((self.day != result_time.day) ? "1 day & " : "") + result_time.strftime("%H :%M :%S").to_s 
+    days = self.day != result_time.day ? 1 : 0
+    return ({ days: days, hours: result_time.hour, mins: result_time.min, secs: result_time.sec })
   end
+
 end
